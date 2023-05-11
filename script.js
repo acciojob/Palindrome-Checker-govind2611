611a94f87ff39061c99d99cc51779715207ff170
Govindsing Chavan <123160733+govind2611@users.noreply.github.com>
2023-05-11 09:15:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 // complete the given function
 
 function palindrome(str){
-
+    str = str.toLowerCase().replace(/[^a-z0-9]/g, "");
+  
+  // Check if the string is the same forwards and backwards
+  for (let i = 0; i < str.length / 2; i++) {
+    if (str[i] !== str[str.length - i - 1]) {
+      return false;
+    }
+  }
+  return true;
 }
 module.exports = palindrome
